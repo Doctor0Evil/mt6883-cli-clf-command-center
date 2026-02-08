4152eaf0f89552236dc4f37e62e1a7ee67e9e6b7
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-07 19:37:04 -0700

    Create lib.rs
    Signed-off-by: Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>

---
+++ engines/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod ai_engine;
+pub mod bio_engine;
+pub mod cloud_sync;
+pub mod data_lake;
+pub mod integrator_chat;
+
+pub use ai_engine::*;
+pub use bio_engine::*;
+pub use cloud_sync::*;
+pub use data_lake::*;
+pub use integrator_chat::*;
